b54f7ba36e8d00fb3ce6c9d6f1c527add0257f10
L1Si <sioulalia@gmail.com>
2023-03-05 22:23:42 +0100

    token element layout

--- src/components/TokenElement.js
+++ src/components/TokenElement.js
@@ -22,10 +22,10 @@ function TokenElement(props){
 
     return (
     <div onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)} className="TokenElement" > 
-       <img className="TokenThumbnail" src={tokenInfo.image } />
-       <span className="tokenElementName">{tokenInfo.name }</span>
-       <span className="tokenElementAmount">{((tokenInfo.decimals) ? Number(props.amount)  / (10**tokenInfo.decimals) : props.amount ).toString()} </span>
-       {/* {showTooltip && <div className="tooltip">{tooltipinfo}</div>} */}
+        <img className="TokenThumbnail" src={tokenInfo.image } />
+        <span className="tokenElementName">{tokenInfo.name }</span>
+        <span className="tokenElementAmount">{((tokenInfo.decimals) ? Number(props.amount)  / (10**tokenInfo.decimals) : props.amount ).toString()} </span>
+        {showTooltip && <div className="tooltip">{tooltipinfo}</div>}
      </div>)
 }
 
