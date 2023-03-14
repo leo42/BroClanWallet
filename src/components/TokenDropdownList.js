4fe8c621298a2e40c55dda5b9c0aa73c45a6d544
leo42 <leantrosh@gmail.com>
2023-03-14 12:54:31 +0100

    added Search to token drop down list

--- src/components/TokenDropdownList.js
+++ src/components/TokenDropdownList.js
@@ -5,7 +5,7 @@ import "./TokenDropdownList.css";
 function TokenDropdownMenu(props) {
     // State to keep track of whether the menu is open or closed
     const [isOpen, setIsOpen] = useState(false);
-    
+    const [search, setSearch] = useState("");
 
     function handleClick(token ){ 
         props.f(token)
@@ -16,14 +16,20 @@ function TokenDropdownMenu(props) {
         <div>
             <button onClick={() => setIsOpen(!isOpen)}>Add Token</button>
             {isOpen && (
+                <div>
+                    <input type="text"  defaultValue={search} onChange={(event) => setSearch(event.target.value)} />
+                    <br/>
                 <div  className="TokenList">
                     {Object.keys(props.ballances).filter((token => token!=="lovelace")).map( (token,index) => 
-                    <div className="TokenListTokenContainer" key={index} > <div ><TokenElement tokenId={token} f={handleClick} amount={props.ballances[token]}  expanded={true}/></div> </div>
+                    <div className="TokenListTokenContainer" key={index} > <div ><TokenElement tokenId={token} f={handleClick} search={search} amount={props.ballances[token]}  expanded={true}/></div> </div>
 
                     )}
                 </div>
+                </div>
+
             )}
         </div>
+        
     );
 }
 
