9707e4eec23ecf4541e17acecb1a796ed9f0df87
leo42 <leantrosh@gmail.com>
2023-03-18 12:32:13 +0100

    Ratelimit and always search for pools

--- src/helpers/SearchPools.js
+++ src/helpers/SearchPools.js
@@ -2,12 +2,11 @@
 async function SearchPools(string){
     const settings = JSON.parse(localStorage.getItem("settings"))
 
-    const networkPrefix = settings.network === "Mainnet" ? "" : settings.network.toLowerCase() + "-"
 
     console.log(settings,string)
-    if (settings.metadataProvider !== "Koios"){
-        return []
-    }
+  //  if (settings.metadataProvider !== "Koios"){
+   //     return []
+   // }
 
     const api = settings.network === "Mainnet" ? "https://api.koios.rest/api/v0/pool_list" : `https://${settings.network}.koios.rest/api/v0/pool_list`
     const responseTicker = await fetch(
@@ -32,7 +31,10 @@ async function SearchPools(string){
     
     const data2 = await responseId.json()
     console.log(data,data2)
-    return data.length !== 0 ? data : data2
+    //return sublist of data that matches string size 10
+        
+    return (data.length !== 0 ? data : data2).slice(0,10)
+
 
     }
 
