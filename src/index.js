8ba3ac024fa44c5113e87385166e16340c8bf458
leo42 <leantrosh@gmail.com>
2023-07-05 17:10:19 +0200

    Added  integrity checks

--- src/index.js
+++ src/index.js
@@ -601,6 +601,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
+      
         <TestnetBanner />
         <ToastContainer
           position="top-right"
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const argv = require('yargs').argv;
+const isProduction = argv.mode === 'production';
+
+
 
 module.exports = {
     entry: './src/index.js',
     output: {
+		crossOriginLoading: 'anonymous',
 			filename: 'bundle.js',
 			path: path.join(__dirname, 'build/public')
     },
@@ -31,15 +38,15 @@ module.exports = {
 			  },
 		]
     },
+	mode : isProduction ? 'production' : 'development',
 	plugins: [
         new CopyWebpackPlugin({
             patterns: [
                 { from: 'src/html', to: '' },
             ],
         }),
+		
     ],
-
-	mode: 'development',
 	devtool : 'source-map',
 	experiments: {
 		asyncWebAssembly: true,
@@ -47,3 +54,24 @@ module.exports = {
 		layers: true // optional, with some bundlers/frameworks it doesn't work without
 		}
 };
+
+if(isProduction) {
+	module.exports.plugins.push(new HtmlWebpackPlugin({
+		template: 'src/index.html' ,
+		hash: true, // Add a hash to the injected script tag
+		inject: 'body', // Inject the script tag in the body of the HTML file
+	  }))
+
+	module.exports.plugins.push(new  SubresourceIntegrityPlugin({
+	hashFuncNames: ['sha256', 'sha384'], // Hash algorithms to use for SRI
+	enabled: true, // Enable SRI generation
+  })) 
+
+}else{
+	module.exports.plugins.push(new CopyWebpackPlugin({
+		patterns: [
+			{ from: 'src/index.dev.html', to: 'index.html' },
+		],
+	}))
+
+}
