9b4fa2a63e84092b1f6622b20cb2f4351e10c508
leo42 <leantrosh@gmail.com>
2023-09-01 04:52:30 +0200

    Added Consumed UTXO checks and variable mint amount

--- src/components/Minting/minting.js
+++ src/components/Minting/minting.js
@@ -22,7 +22,7 @@ class Minting extends React.Component {
     }     
     
     
-    func paymentMade (ctx: ScriptContext ) -> Bool {
+    func paymentMade (ctx: ScriptContext , amount : Int ) -> Bool {
     
         adminUtxo: TxInput = ctx.tx.ref_inputs.find((input: TxInput) -> Bool { 
                                                                input.value.get_safe(AdminToken) == 1
@@ -30,23 +30,36 @@ class Minting extends React.Component {
         
         adminDatum : Map[String]Int = Map[String]Int::from_data(adminUtxo.datum.get_inline_data()); 
     
-        
-    
         paymentUtxo: TxOutput = ctx.tx.outputs.find(( output: TxOutput) -> Bool {
                                                         output.address == adminUtxo.address
-                                                         })    ;
+                                                         });
     
-        paymentUtxo.value.get_lovelace() == adminDatum.get("paymentAmount")
+      
+        paymentUtxo.value.get_lovelace() == adminDatum.get("paymentAmount") * amount
     
+    }
+    
+    
+    func mintCorrect(ctx: ScriptContext ) -> Bool {
+        mintingAssets : Map[ByteArray]Int = ctx.tx.minted.to_map().get(ctx.get_current_minting_policy_hash());
         
+     
+         mintingAssets.all( ( mintedToken : ByteArray, _)  -> Bool { 
+                                    ctx.tx.inputs.any((input : TxInput) -> Bool {  input.output_id == TxOutputId::new(TxId::new(mintedToken), 0)}
+                    )})   
+       
     }
     
     
-    
-    func main(_, ctx: ScriptContext) -> Bool {
-         
-         paymentMade(ctx )
+    func mintAmount(ctx: ScriptContext) -> Int {
+        mintingAssets : Map[ByteArray]Int = ctx.tx.minted.to_map().get(ctx.get_current_minting_policy_hash());
+        mintingAssets.length 
     }
+    
+    func main( _ , ctx: ScriptContext) -> Bool {
+         paymentMade(ctx , mintAmount(ctx)) && mintCorrect(ctx )
+    }
+    
     `      
 
     render() {
