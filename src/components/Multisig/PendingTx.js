52e6a2186dc71818275c20d6a4fe16970d833da3
leo42 <leantrosh@gmail.com>
2023-09-08 00:47:24 +0200

    validator Complete (inlcuding  afiliate bounty)

--- src/components/Multisig/PendingTx.js
+++ src/components/Multisig/PendingTx.js
@@ -272,7 +272,7 @@ function WalletPendingTx(props) {
 
 
             <svg onClick={copyTransaction} className="copyIcon" id="meteor-icon-kit__solid-copy-s" fill="none"><path fillRule="evenodd" clipRule="evenodd" d="M7 5H14C15.1046 5 16 5.89543 16 7V14C16 15.1046 15.1046 16 14 16H7C5.89543 16 5 15.1046 5 14V7C5 5.89543 5.89543 5 7 5zM3 11H2C0.89543 11 0 10.1046 0 9V2C0 0.89543 0.89543 0 2 0H9C10.1046 0 11 0.89543 11 2V3H7C4.79086 3 3 4.79086 3 7V11z" fill="#758CA3"/></svg>
-            {txDetails.certs && txDetails.certs.some(obj => "StakeDelegation" in obj)  ? "Delegation Transaction" : "Regular Transaxtion"} 
+            {txDetails.certs && txDetails.certs.some(obj => "StakeDelegation" in obj)  ? "Delegation Transaction" : "Regular Transaction"} 
              
              <br/>
             {inputUtxos.length !== 0 ? transactionBalance(txDetails) : ""}
