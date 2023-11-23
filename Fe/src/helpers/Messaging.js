a052b8168fb94015e3b14c6c49a4d7d0e0fd6537
leo42 <leantrosh@gmail.com>
2023-11-23 18:46:29 +0200

    Proper serialization of Utxos

--- Fe/src/helpers/Messaging.js
+++ Fe/src/helpers/Messaging.js
@@ -1,6 +1,13 @@
 // Desc: Messaging helper for communicating with background script
 // Usage: Messaging(this);
 //
+import { Lucid, utxoToCore  } from "lucid-cardano";
+
+function toHexString(byteArray) {
+    return Array.from(byteArray, function(byte) {
+      return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+  }
 
 class Messaging {   
 
@@ -8,10 +15,12 @@ class Messaging {
         this.wallet = wallet;
         this.port = null;
         this.connect();
+
     }
 
 
-    connect() {
+   async connect() {
+
         this.port = chrome.runtime.connect("jfjmokidpopgdhcilhkoanmjcimijgng");
         this.port.onMessage.addListener((message) => {
             console.log("Received message from background script:", message);
@@ -23,7 +32,7 @@ class Messaging {
                         break;
                     case "getUtxos":
                         
-                        this.port.postMessage({ response: JSON.stringify(this.wallet.getUtxos().length) });
+                        this.port.postMessage({ response: this.wallet.getUtxos().map((utxo) => ( toHexString(utxoToCore(utxo).to_bytes()))) });
                         break;    
                 }
              }
