3432d18a4fcf620e26cd74010601dae2937a12e7
leo42 <leantrosh@gmail.com>
2024-09-12 16:35:42 +0300

    Sucsessfully merge typscript into the project

---
+++ Fe/src/components/SmartWallet/SmartWalletContainer.tsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+
+interface SmartWalletContainerProps {
+  settings: any;
+  root: any;
+}
+
+
+interface SmartWalletContainerState {
+  modal: string;
+  wallets: any[];
+  selectedWallet: number;
+  connectedWallet: { name: string; socket: any };
+  loading: boolean;
+  dAppConnector: any | null;
+}
+
+class SmartWalletContainer extends React.Component<SmartWalletContainerProps, SmartWalletContainerState> {
+  private interval: NodeJS.Timeout | null = null;
+
+  state: SmartWalletContainerState = {
+    modal: "",
+    wallets: [],
+    selectedWallet: 0,
+    connectedWallet: { name: "", socket: null },
+    loading: true,
+    dAppConnector: null,
+  };
+
+  componentDidMount() {
+    this.loadState();
+    this.interval = setInterval(() => {
+      this.reloadBalance();
+    }, 15000);
+  }
+
+  componentWillUnmount() {
+    if (this.state.dAppConnector) {
+      this.state.dAppConnector.disconnect();
+    }
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  componentDidUpdate(prevProps: SmartWalletContainerProps) {
+    if (this.props.settings !== prevProps.settings) {
+      this.newSettings(this.props.settings);
+    }
+  }
+
+  async newSettings(newSettings: any) {
+    // Implementation similar to MultisigContainer
+  }
+
+  async showModal(modalName: string) {
+    this.setState({ modal: modalName });
+  }
+
+  async connectWallet(wallet: string) {
+    // Implementation similar to MultisigContainer
+  }
+
+  disconnectWallet(error: string = "") {
+    // Implementation similar to MultisigContainer
+  }
+
+  async reloadBalance() {
+    // Implementation similar to MultisigContainer
+  }
+
+  storeState() {
+    // Implementation similar to MultisigContainer
+  }
+
+  storeWallets() {
+    // Implementation similar to MultisigContainer
+  }
+
+  async loadState() {
+    // Implementation similar to MultisigContainer
+  }
+
+  modalType() {
+    return "smart";
+  }
+
+  async createTx(recipients: any[], signers: any[], sendFrom: any, sendAll: boolean | null = null) {
+    // Implementation similar to MultisigContainer
+  }
+
+  async importTransaction(transaction: any) {
+    // Implementation similar to MultisigContainer
+  }
+
+  async deleteWallet(index: number) {
+    // Implementation similar to MultisigContainer
+  }
+
+  changeWalletName(name: string) {
+    // Implementation similar to MultisigContainer
+  }
+
+  addSignature(signature: any) {
+    // Implementation similar to MultisigContainer
+  }
+
+  setDefaultAddress(address: string) {
+    // Implementation similar to MultisigContainer
+  }
+
+  changeAddressName(address: string, name: string) {
+    // Implementation similar to MultisigContainer
+  }
+
+  getTransactionHistory(address: string) {
+    // Implementation similar to MultisigContainer
+  }
+
+  async addWallet(script: any, name: string) {
+    // Implementation similar to MultisigContainer
+  }
+
+  loadWallets() {
+    // Implementation similar to MultisigContainer
+  }
+
+  transmitTransaction(transaction: any, sigAdded: any) {
+    // Implementation similar to MultisigContainer
+  }
+
+  transmitWallet(script: any) {
+    // Implementation similar to MultisigContainer
+  }
+
+  async loadTransaction(transaction: any, walletIndex: number) {
+    // Implementation similar to MultisigContainer
+  }
+
+  selectWallet(key: number) {
+    // Implementation similar to MultisigContainer
+  }
+
+  async submit(index: number) {
+    // Implementation similar to MultisigContainer
+  }
+
+  walletsEmpty() {
+    return (
+     <div className="walletsEmpty">
+        <h2>No Smart Wallets Found</h2>
+        <p>Create a new smart wallet to start using this APP.</p>
+        <button className="commonBtn" onClick={() => this.setState({modal: "newWallet"})}>Add Smart Wallet</button>
+        Malakaaaaa
+      </div>
+    );
+  }
+  
+  render() {
+    return (
+      <div className="SmartWalletContainer"> Hey Leo
+        {this.state.wallets.length === 0 ? this.walletsEmpty() : (
+         <div> Hello World</div> // Render your wallet list or other content here
+        )}
+      </div>
+    );
+  }
+  
+}
+
+export default SmartWalletContainer;
--- Fe/webpack.config.js
+++ Fe/webpack.config.js
@@ -5,8 +5,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const argv = require('yargs').argv;
 const isProduction = argv.mode === 'production';
 
-
-
 var webAppConfig = {
     entry: './src/index.js',
     output: {
@@ -36,6 +34,11 @@ var webAppConfig = {
 				test: /\.css$/i,
 				use: ["style-loader", "css-loader"],
 			  },
+				{
+					test: /\.(ts|tsx)$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
 		]
     },
 	mode : isProduction ? 'production' : 'development',
@@ -57,7 +60,6 @@ var webAppConfig = {
 		layers: true // optional, with some bundlers/frameworks it doesn't work without
 		}
 };
-
 
 var extensionConfig = {
 	entry:  './src/extension/extension.js',
@@ -87,8 +89,8 @@ var extensionConfig = {
 			test: /\.css$/i,
 			use: ["style-loader", "css-loader"],
 		  },
-	]
-},
+		]
+	},
 	  plugins: [
 		new CopyWebpackPlugin({
 			patterns: [
@@ -186,7 +188,6 @@ var workerConfig = {
 				}
 		};	
 	
-
 module.exports = [webAppConfig, extensionConfig , workerConfig, injectionConfig, approvalConfig]
 if(isProduction) {
 	module.exports[0].plugins.push(new HtmlWebpackPlugin({
