bcf904943824ed6945ead856cd30a50f82638b3a
leo42 <leantrosh@gmail.com>
2024-09-17 00:57:18 +0300

    midpoint

--- Fe/src/components/SmartWallet/types.ts
+++ Fe/src/components/SmartWallet/types.ts
@@ -2,8 +2,8 @@ import {  Data } from "@lucid-evolution/lucid";
 
 
 export const adminDatumSchema = Data.Object({
-  mintAmount: Data.Integer(),
-  paymentAddressCredential: Data.Bytes(),
+  mintAmount: Data.Integer({minimum: 0}),
+  paymentAddressCredential: Data.Bytes({minLength: 28, maxLength: 28}),
 });
 
 export type AdminDatum = Data.Static<typeof adminDatumSchema>;
