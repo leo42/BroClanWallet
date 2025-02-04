3faa0f2359e1b332ab0293fbdfc4cf11a6face82
leo42 <leantrosh@gmail.com>
2025-02-04 13:55:29 +0200

    Refactor PendingTxs

--- Fe/src/components/PendingTxs.tsx
+++ Fe/src/components/PendingTxs.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import WalletPendingTx from "./PendingTx";
 import { useState, useEffect } from "react";    
 import { toast } from 'react-toastify';
+import WalletInterface from "./WalletInterface";
 
-function PendingTxs(props){
+type PendingTxsProps = {
+    moduleRoot: any;
+    root: any;
+    wallet: WalletInterface;
+}
+
+function PendingTxs(props: PendingTxsProps){
+
 
     const [importTransaction, setImportTransaction] = useState(false);
     const [importedTx, setImportedTx] = useState("");
 
-    const  importTx = (importedTx) =>{
+    const  importTx = (importedTx: string) =>{
         try{
             props.moduleRoot.importTransaction(importedTx);
             setImportTransaction(false);
-        }catch(error){
+        }catch(error: any){
             toast.error("Error importing transaction: " + error.message);
         }
+
 
     }
 
@@ -22,9 +31,10 @@ function PendingTxs(props){
     return(
         <div className="pendingTxs"  key={pending}>
             <h1>Pending Transactions</h1>
-            {props.wallet.getPendingTxs().map( (pendingTx, index) => (
-                <WalletPendingTx moduleRoot={props.moduleRoot} root={props.root} index={index} tx={pendingTx}  wallet={props.wallet}  key={pendingTx.tx.toString()}></WalletPendingTx>
+            {props.wallet.getPendingTxs().map( (pendingTx: any, index: number) => (
+                <WalletPendingTx moduleRoot={props.moduleRoot} root={props.root} index={index} tx={pendingTx}  wallet={props.wallet}  key={pendingTx.tx.toCBOR()}></WalletPendingTx>
                 ) )}
+
             <button className="commonBtn" onClick={() =>setImportTransaction(!importTransaction)}>Import Transaction</button>
             {importTransaction ? <div>
                     <input type="text" defaultValue={importedTx} onChange={(event)=> setImportedTx(event.target.value)} placeholder="Transaction Data"></input>
